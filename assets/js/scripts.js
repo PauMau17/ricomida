cb5acfb654d698c12fbaab2bdbdcf7c30bc26b43
Paula <maureiraisla@gmail.com>
2020-01-27 04:28:58 -0300

    subiendo RICOMIDA

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+//comentario de una sola linea
+/* comentario en bloque */
+
+
+$(document).ready;
+
+$('#example').tooltip(options)
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+p
